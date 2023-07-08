3edec3a5c6d76bc8d9f47ed4eea3342c15a4b191
Abhishek-data <abhishekkr1915@gmail.com>
2023-07-08 09:06:20 +0530

    set up style mart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {} from "antd";
+import Header from "./component/header";
+import PageContent from "./component/pageContent";
+import Footer from "./component/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <PageContent/>
+      <Footer/>
     </div>
   );
 }
